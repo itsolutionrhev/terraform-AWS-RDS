05d65ec3f7685015de6c1f75452b3336d4f418a2
itsolutionrhev <vishal.ns.patel@gmail.com>
2021-01-31 14:51:46 +0530

    update

--- AWS-RDS/createInstance.tf
+++ AWS-RDS/createInstance.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "itsolutionrhev_key" {
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = "ap-south-1"
+  availability_zone = "ap-south-1a"
   key_name      = aws_key_pair.itsolutionrhev_key.key_name
   vpc_security_group_ids = [aws_security_group.allow-itsolutionrhev-ssh.id]
   subnet_id = aws_subnet.itsolutionrhevvpc-public-1.id
